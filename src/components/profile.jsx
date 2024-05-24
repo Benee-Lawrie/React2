7da9f20a0c78b9aca9cad9aa113962992b400075
Benedicta Lawrence <bennywave21@gmail.com>
2024-05-24 15:06:22 +0100

    first commit

---
+++ src/components/profile.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import User from './User';
+
+const Profile = ({ users }) => {
+  const [localUsers, setLocalUsers] = useState([]);
+
+  useEffect(() => {
+     setTimeout(() => {
+      setLocalUsers(users);
+     }, 2000); 
+  }, []);
+
+  return (
+    <div className="body">
+      {localUsers.map((user) => (
+        <User key={user.Name} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default Profile;
